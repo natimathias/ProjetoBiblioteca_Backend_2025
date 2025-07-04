650a4a85c921f225cadaa06343fc2ca86884abb1
natimathias <130069163+natimathias@users.noreply.github.com>
2025-07-03 23:08:32 -0300

    Rota puxada no catalogo

--- model/livro.dao.js
+++ model/livro.dao.js
@@ -5,8 +5,8 @@ exports.cadastrarLivro = async function (novo_livro) {
     const extensao_arquivo = novo_produto.imagem.name.split(".").pop();
 
     const resposta = await db.query(
-        'INSERT INTO livro (nome, titulo, qt_disponivel, isbn, id_autores, edicao, id_editora, caminho_imagens) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning id_livro',
-        [novo_livro.nome, novo_livro.tiulo, novo_livro.qt_disponivel, novo_livro.isbn, novo_livro.id_autores, novo_livro.edicao, novo_livro.id_edicao, novo_livro.caminho_imagens, extensao_arquivo]
+        'INSERT INTO livro (titulo, qt_disponivel, isbn, id_autores, edicao, id_editora, caminho_imagens) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) returning id_livro',
+        [novo_livro.titulo, novo_livro.qt_disponivel, novo_livro.isbn, novo_livro.id_autores, novo_livro.edicao, novo_livro.id_edicao, novo_livro.caminho_imagens, extensao_arquivo]
     );
 
     return resposta.rows[0].id_livro;
